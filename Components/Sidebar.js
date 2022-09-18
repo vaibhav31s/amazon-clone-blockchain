6744109e5ad247b51473099fa8de6c1e731fb391
vaibhav31s <58821506+vaibhav31s@users.noreply.github.com>
2022-09-19 01:39:00 +0530

    less go

--- Components/Sidebar.js
+++ Components/Sidebar.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import {ConnectButton} from "web3uikit";
+import { Stack } from '@mui/system';
 
+const styles = {
+  container : `h-full w-full flex`,
+  profiele:  `h-full w-full flex `
+}
+const isAuthenticated = true;
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <>
+  
+        <div className={styles.container}>
+      <div className={styles.profile}>
+        {
+          isAuthenticated &&  (
+            <Stack>
+                  Hellow
+            </Stack>
+       
+            
+          ) ? <Stack> Word</Stack> : <Stack> jwergf</Stack>
+  
+        }
+      </div>
+    </div>
+    </>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../Components/Sidebar'
-import {ConnectButton} from "web3uikit";
-const styles = {
-  container : `h-full w-full flex bg-[#fff]`
-}
+import { Paper,Box } from '@mui/material'
+import { Avatar } from 'web3uikit'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div >
+      <Box
+      sx={{
+        display: 'flex',
+        '& > :not(style)': {
+          m: 1,
+          width: 260,
+          height: 328,
+          // bgcolor: 'primary.main' ,
+         borderRadius:12,
+         boxShadow: 21
+        },
+      }}
+    >
+
+      <Paper sx={{justifyContent: 'space-between'}} variant="outlined" square >
+      <Avatar
+        sx={{ bgcolor: deepOrange[500] }}
+        alt="Remy Sharp"
+        src="/broken-image.jpg"
+      />
+      </Paper>
+    </Box>
       <Sidebar/>
 
-      <ConnectButton />      {/* <Main/> */}
+           {/* <Main/> */}
     </div>
   )
 }
