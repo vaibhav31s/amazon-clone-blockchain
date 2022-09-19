c65d34f0f2f31dcee50813510982cdac78b6d2e3
vaibhav31s <58821506+vaibhav31s@users.noreply.github.com>
2022-09-20 02:50:38 +0530

    added complete side bar with authentication hooks

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import '../styles/globals.css'
 import { MoralisProvider } from "react-moralis";
+import { AmazonProvider } from "../Context/AmazonContexts"
 function MyApp({ Component, pageProps }) {
-  return <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} appId={process.env.NEXT_PUBLIC_MORALIS_API}>
-    <Component {...pageProps} />
-    </MoralisProvider>
+  return (
+  <MoralisProvider serverUrl={process.env.NEXT_PUBLIC_MORALIS_SERVER} appId={process.env.NEXT_PUBLIC_MORALIS_API}>
+    <AmazonProvider>
+        <Component {...pageProps} />
+    </AmazonProvider>
+  </MoralisProvider>);
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Sidebar from '../Components/Sidebar'
-import { Paper,Box } from '@mui/material'
-import { Avatar } from 'web3uikit'
+
 
 export default function Home() {
   return (
     <div >
-      <Box
-      sx={{
-        display: 'flex',
-        '& > :not(style)': {
-          m: 1,
-          width: 260,
-          height: 328,
-          // bgcolor: 'primary.main' ,
-         borderRadius:12,
-         boxShadow: 21
-        },
-      }}
-    >
-
-      <Paper sx={{justifyContent: 'space-between'}} variant="outlined" square >
-      <Avatar
-        sx={{ bgcolor: deepOrange[500] }}
-        alt="Remy Sharp"
-        src="/broken-image.jpg"
-      />
-      </Paper>
-    </Box>
+     
       <Sidebar/>
 
            {/* <Main/> */}
